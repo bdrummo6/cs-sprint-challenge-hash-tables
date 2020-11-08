6d238cf1e3842c0f7879cc36e3f585c56b3bb14b
bdrummo6 <bradley.drummonds@gmail.com>
2020-11-08 18:39:52 -0500

    Added comments

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,17 +9,22 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    # dict for holding the the source and destination for each leg of the trip
     route_dict = {}
 
+    # Create an array to hold each place in your trip
     trip = [None] * length
 
-    for route in tickets:
-        route_dict[route.source] = route.destination
+    for ticket in tickets:
+        route_dict[ticket.source] = ticket.destination
 
-    locale = route_dict['NONE']
+    # Set the initial source to next_source
+    next_source = route_dict['NONE']
 
+    # iterate through the length of the trip storing each location in order from first to last in the array trip
     for stop in range(length):
-        trip[stop] = locale
-        locale = route_dict[locale]
+        trip[stop] = next_source
+        next_source = route_dict[next_source]
 
+    # return the array of places on your trip
     return trip
